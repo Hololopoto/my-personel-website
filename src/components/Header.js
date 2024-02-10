adadd7690388c930985d5bd6fce15457e65bf26f
Alper Demir <105297896+Hololopoto@users.noreply.github.com>
2024-02-10 15:14:02 +0300

    Gereksiz dosyaların silinmesi, package.json güncellemeleri ve yeni bağımlılıklar eklendi.

---
+++ src/components/Header.js
@@ -0,0 +1,39 @@
+import { React, useEffect, useState } from "react";
+
+function App() {
+  const [menu, setMenu] = useState(false);
+  const handleMenu = () => {
+    setMenu(!menu);
+  };
+  return (
+    <div className="App bg-slate-300">
+      <div className="container  ">
+        <div className="items h-20 flex flex-row items-center text-white">
+          <button
+            className=" px-5 py-3 m-9 rounded-md bg-zinc-700"
+            onClick={handleMenu}>
+            İletişim
+          </button>
+          {menu ? (
+            <div className="link bg-slate-300   transition ">
+              <ul className="flex  flex-row gap-7">
+                <li className="bg-zinc-700 cursor-pointer hover:bg-gray-700 py-1 px-3 rounded-md">
+                  <a href="http://">Linkedin</a>
+                </li>
+                <li className="bg-zinc-700 cursor-pointer hover:bg-gray-700 py-1 px-3 rounded-md">
+                  GitHub
+                </li>
+              </ul>
+            </div>
+          ) : null}
+
+          <div className="logo">
+            <img src="" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Talents.js
@@ -0,0 +1,75 @@
+import React from "react";
+
+function Talents() {
+  return (
+    <div className="box-border flex flex-col gap-9">
+      <div className="maincontain flex h-auto box-border  bg-red-400">
+        <div className="container mx-20  h-64 w-[50%]  gap-3 flex flex-col items-center justify-center bg-white">
+          <div className="img">
+            <img
+              className="h-36"
+              src="https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg"
+              alt="HTML5"
+            />
+          </div>
+          <div className=" ">
+            <h1>HTML</h1>
+          </div>
+        </div>
+        <div className="container mx-20 h-64 w-[50%] box-border  gap-3 flex flex-col items-center justify-center bg-white">
+          <div className="img">
+            <img
+              className="h-36"
+              src="https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg"
+              alt="HTML5"
+            />
+          </div>
+          <div className=" ">
+            <h1>HTML</h1>
+          </div>
+        </div>
+      </div>
+      {/* 3Lü Satır */}
+      <div className="maincontain flex h-auto box-border  bg-red-400">
+        <div className="container mx-20  h-64 w-[50%]  gap-3 flex flex-col items-center justify-center bg-white">
+          <div className="img">
+            <img
+              className="h-36"
+              src="https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg"
+              alt="HTML5"
+            />
+          </div>
+          <div className=" ">
+            <h1>HTML</h1>
+          </div>
+        </div>
+        <div className="container mx-20 h-64 w-[50%] box-border  gap-3 flex flex-col items-center justify-center bg-white">
+          <div className="img">
+            <img
+              className="h-36"
+              src="https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg"
+              alt="HTML5"
+            />
+          </div>
+          <div className=" ">
+            <h1>HTML</h1>
+          </div>
+        </div>
+        <div className="container mx-20 h-64 w-[50%] box-border  gap-3 flex flex-col items-center justify-center bg-white">
+          <div className="img">
+            <img
+              className="h-36"
+              src="https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg"
+              alt="HTML5"
+            />
+          </div>
+          <div className=" ">
+            <h1>HTML</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Talents;
