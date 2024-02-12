24ec1ada94580ce2bcc6822ebacd3577ba893efe
Alper Demir <105297896+Hololopoto@users.noreply.github.com>
2024-02-12 11:37:26 +0300

    update logos

--- src/components/Talents.js
+++ src/components/Talents.js
@@ -34,7 +34,7 @@ function Talents() {
           <div className="img">
             <img
               className="h-36"
-              src="https://upload.wikimedia.org/wikipedia/commons/b/ba/Javascript_badge.svg"
+              src="https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg"
               alt="HTML5"
             />
           </div>
@@ -46,7 +46,7 @@ function Talents() {
           <div className="img">
             <img
               className="h-36"
-              src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
+              src="https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg"
               alt="HTML5"
             />
           </div>
