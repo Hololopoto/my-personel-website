ab597cace749acce9ab7fb10300541b15178fa90
Alper Demir <105297896+Hololopoto@users.noreply.github.com>
2024-02-13 15:00:50 +0300

    İmg's updates

--- src/components/Talents.js
+++ src/components/Talents.js
@@ -4,7 +4,7 @@ function Talents() {
   return (
     <div className="box-border flex flex-col gap-9">
       <div className="maincontain flex h-auto box-border  bg-red-400">
-        <div className="container mx-20  h-64 w-[50%]  gap-3 flex flex-col items-center justify-center bg-white">
+        <div className="container mx-20  h-64 w-[50%]  gap-3 flex flex-col items-center justify-center rounded-lg bg-white">
           <div className="img">
             <img
               className="h-36"
@@ -13,10 +13,10 @@ function Talents() {
             />
           </div>
           <div className=" ">
-            <h1>HTML</h1>
+            <h1>Javascript</h1>
           </div>
         </div>
-        <div className="container mx-20  h-64 w-[50%]  gap-3 flex flex-col items-center justify-center bg-white">
+        <div className="container mx-20  h-64 w-[50%]  gap-3 flex flex-col items-center justify-center rounded-lg bg-white">
           <div className="img">
             <img
               className="h-36"
@@ -25,12 +25,12 @@ function Talents() {
             />
           </div>
           <div className=" ">
-            <h1>HTML</h1>
+            <h1>React</h1>
           </div>
         </div>
       </div>
       <div className="maincontain flex h-auto box-border  bg-red-400">
-        <div className="container mx-20  h-64 w-[50%]  gap-3 flex flex-col items-center justify-center bg-white">
+        <div className="container mx-20  h-64 w-[50%]  gap-3 flex flex-col items-center justify-center rounded-lg bg-white">
           <p>Text Content</p>
           <div className="img">
             <img
@@ -40,10 +40,10 @@ function Talents() {
             />
           </div>
           <div className=" ">
-            <h1>HTML</h1>
+            <h1>Css</h1>
           </div>
         </div>
-        <div className="container mx-20  h-64 w-[50%]  gap-3 flex flex-col items-center justify-center bg-white">
+        <div className="container mx-20  h-64 w-[50%]  gap-3 flex flex-col items-center justify-center rounded-lg bg-white">
           <div className="img">
             <img
               className="h-36"
@@ -52,17 +52,29 @@ function Talents() {
             />
           </div>
           <div className=" ">
-            <h1>HTML</h1>
+            <h1>Html</h1>
           </div>
         </div>
       </div>
       {/* 3Lü Satır */}
       <div className="maincontain flex h-auto box-border  bg-red-400">
-        <div className="container mx-20  h-64 w-[50%]  gap-3 flex flex-col items-center justify-center bg-white">
+        <div className="container mx-20  h-64 w-[50%]  gap-3 flex flex-col items-center justify-center rounded-lg bg-white">
           <div className="img">
             <img
-              className="h-36"
-              src="https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg"
+              className="h-28 w-auto hover:scale-125 transition"
+              src="https://devnot.com/wp-content/uploads/2017/04/jsonapi.png"
+              alt="HTML5"
+            />
+          </div>
+          <div className=" ">
+            <h1>Rest Api with Json Server</h1>
+          </div>
+        </div>
+        <div className="container mx-20 h-64 w-[50%] box-border  gap-3 flex flex-col items-center justify-center rounded-lg bg-white">
+          <div className="img">
+            <img
+              className="h-28  w-auto hover:scale-125 transition"
+              src="https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg"
               alt="HTML5"
             />
           </div>
@@ -70,22 +82,10 @@ function Talents() {
             <h1>HTML</h1>
           </div>
         </div>
-        <div className="container mx-20 h-64 w-[50%] box-border  gap-3 flex flex-col items-center justify-center bg-white">
+        <div className="container mx-20 h-64 w-[50%] box-border  gap-3 flex flex-col items-center justify-center rounded-lg bg-white">
           <div className="img">
             <img
-              className="h-36"
-              src="https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg"
-              alt="HTML5"
-            />
-          </div>
-          <div className=" ">
-            <h1>HTML</h1>
-          </div>
-        </div>
-        <div className="container mx-20 h-64 w-[50%] box-border  gap-3 flex flex-col items-center justify-center bg-white">
-          <div className="img">
-            <img
-              className="h-36"
+              className="h-28 w-auto hover:scale-125 transition"
               src="https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg"
               alt="HTML5"
             />
