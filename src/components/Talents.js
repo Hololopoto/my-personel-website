e42fafad54415f06f87f49797e37761de378fa6a
Alper Demir <105297896+Hololopoto@users.noreply.github.com>
2024-02-14 12:55:47 +0300

    Update function to handle edge case

--- src/components/Talents.js
+++ src/components/Talents.js
@@ -2,55 +2,55 @@ import React from "react";
 
 function Talents() {
   return (
-    <div className="box-border flex flex-col gap-9">
+    <div className="box-border flex flex-col gap-6">
       <div className="maincontain flex h-auto box-border  bg-red-400">
-        <div className="container mx-20  h-64 w-[50%] hover:scale-110 cursor-pointer transition gap-3  rounded-lg bg-white">
+        <div className="container mx-10  h-64 w-[50%] hover:scale-110 cursor-pointer transition gap-3  rounded-lg bg-white">
           <div className="float-left m-3 absolute">
-            <p className=" text-lg">Text Content</p>
+            <p className=" text-lg">Programing Language</p>
           </div>
 
           <div className="img mt-12 flex justify-center items-center">
             <img
               className="h-36 hover:scale-125 transition"
-              src="https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg"
-              alt="HTML5"
+              src="https://upload.wikimedia.org/wikipedia/commons/b/ba/Javascript_badge.svg"
+              alt="js"
             />
           </div>
 
           <div className="float-right  m-6">
-            <h1>Css</h1>
+            <h1>Javascript</h1>
           </div>
         </div>
-        <div className="container mx-20  h-64 w-[50%] hover:scale-110 cursor-pointer transition gap-3  rounded-lg bg-white">
+        <div className="container mx-10  h-64 w-[50%] hover:scale-110 cursor-pointer transition gap-3  rounded-lg bg-white">
           <div className="float-left m-3 absolute">
-            <p className=" text-lg">Text Content</p>
+            <p className=" text-lg">Javascript Library</p>
           </div>
 
           <div className="img mt-12 flex justify-center items-center">
             <img
               className="h-36 hover:scale-125 transition"
-              src="https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg"
-              alt="HTML5"
+              src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
+              alt="React"
             />
           </div>
 
           <div className="float-right  m-6">
-            <h1>Css</h1>
+            <h1>React</h1>
           </div>
         </div>
       </div>
 
       <div className="maincontain flex h-auto box-border  bg-red-400">
-        <div className="container mx-20  h-64 w-[50%] hover:scale-110 cursor-pointer transition gap-3  rounded-lg bg-white">
+        <div className="container mx-10  h-64 w-[50%] hover:scale-110 cursor-pointer transition gap-3  rounded-lg bg-white">
           <div className="float-left m-3 absolute">
-            <p className=" text-lg">Text Content</p>
+            <p className=" text-lg">Style Markup Language</p>
           </div>
 
           <div className="img mt-12 flex justify-center items-center">
             <img
               className="h-36 hover:scale-125 transition"
               src="https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg"
-              alt="HTML5"
+              alt="Css"
             />
           </div>
 
@@ -58,60 +58,128 @@ function Talents() {
             <h1>Css</h1>
           </div>
         </div>
-        <div className="container mx-20  h-64 w-[50%] hover:scale-110 cursor-pointer transition gap-3  rounded-lg bg-white">
+        <div className="container mx-10  h-64 w-[50%] hover:scale-110 cursor-pointer transition gap-3  rounded-lg bg-white">
           <div className="float-left m-3 absolute">
-            <p className=" text-lg">Text Content</p>
+            <p className=" text-lg">Text Markup Language</p>
           </div>
 
           <div className="img mt-12 flex justify-center items-center">
             <img
               className="h-36 hover:scale-125 transition"
-              src="https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg"
+              src="https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg"
               alt="HTML5"
             />
           </div>
 
           <div className="float-right  m-6">
-            <h1>Css</h1>
+            <h1>Html</h1>
           </div>
         </div>
       </div>
       {/* 3Lü Satır */}
-      <div className="maincontain flex h-auto  bg-red-400">
-        <div className="container mx-10  h-64 mb-10  hover:scale-110 cursor-pointer transition gap-3 flex flex-col items-center justify-center rounded-lg bg-white">
-          <div className="img">
+      <div className="maincontain flex h-auto box-border  bg-red-400">
+        <div className="container mx-10  h-64 w-[50%] hover:scale-110 cursor-pointer transition gap-3  rounded-lg bg-white">
+          <div className="float-left m-3 absolute">
+            <p className=" text-lg">API</p>
+          </div>
+
+          <div className="img mt-12 flex justify-center items-center">
             <img
-              className="h-28 w-auto hover:scale-125 transition cursor-pointer"
+              className="h-36 hover:scale-125 transition"
               src="https://devnot.com/wp-content/uploads/2017/04/jsonapi.png"
-              alt="HTML5"
+              alt="API"
             />
           </div>
-          <div className=" ">
-            <h1>Rest Api with Json Server</h1>
+
+          <div className="float-right  m-6">
+            <h1>Rest API with JSON Server</h1>
           </div>
         </div>
-        <div className="container mx-20 h-64  box-border hover:scale-110 cursor-pointer transition  gap-3 flex flex-col items-center justify-center rounded-lg bg-white">
-          <div className="img">
+        <div className="container mx-10  h-64 w-[50%] hover:scale-110 cursor-pointer transition gap-3  rounded-lg bg-white">
+          <div className="float-left m-3 absolute">
+            <p className=" text-lg">Css Framework</p>
+          </div>
+
+          <div className="img mt-12 flex justify-center items-center">
             <img
-              className="h-28  w-auto hover:scale-125 transition cursor-pointer"
+              className="h-36 hover:scale-125 transition"
               src="https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg"
               alt="HTML5"
             />
           </div>
-          <div className=" ">
+
+          <div className="float-right  m-6">
             <h1>Tailwind</h1>
           </div>
         </div>
-        <div className="container mx-20 h-64  box-border hover:scale-110 cursor-pointer transition  gap-3 flex flex-col items-center justify-center rounded-lg bg-white">
-          <div className="img">
+        <div className="container mx-10  h-64 w-[50%] hover:scale-110 cursor-pointer transition gap-3  rounded-lg bg-white">
+          <div className="float-left m-3 absolute">
+            <p className=" text-lg">Css Framework</p>
+          </div>
+
+          <div className="img mt-12 flex justify-center items-center">
             <img
-              className="h-28 w-auto hover:scale-125 transition cursor-pointer"
+              className="h-36 hover:scale-125 transition"
               src="https://upload.wikimedia.org/wikipedia/commons/b/b2/Bootstrap_logo.svg"
               alt="HTML5"
             />
           </div>
-          <div className=" ">
+
+          <div className="float-right  m-6">
             <h1>Bootstrap</h1>
+          </div>
+        </div>
+      </div>
+      <div className="maincontain flex h-auto box-border  bg-red-400">
+        <div className="container mx-10  h-64 w-[50%] hover:scale-110 cursor-pointer transition gap-3  rounded-lg bg-white">
+          <div className="float-left m-3 absolute">
+            <p className=" text-lg">Responsive Design</p>
+          </div>
+
+          <div className="img mt-12 flex justify-center items-center">
+            <img
+              className="h-36 hover:scale-125 transition"
+              src="https://upload.wikimedia.org/wikipedia/commons/9/9a/Responsive.png"
+              alt="HTML5"
+            />
+          </div>
+
+          <div className="float-right  m-6">
+            <h1>Mobile and Desktop Design</h1>
+          </div>
+        </div>
+        <div className="container mx-10  h-64 w-[50%] hover:scale-110 cursor-pointer transition gap-3  rounded-lg bg-white">
+          <div className="float-left m-3 absolute">
+            <p className=" text-lg">Text Content</p>
+          </div>
+
+          <div className="img mt-12 flex justify-center items-center">
+            <img
+              className="h-36 hover:scale-125 transition"
+              src="https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg"
+              alt="HTML5"
+            />
+          </div>
+
+          <div className="float-right  m-6">
+            <h1>Css</h1>
+          </div>
+        </div>
+        <div className="container mx-10  h-64 w-[50%] hover:scale-110 cursor-pointer transition gap-3  rounded-lg bg-white">
+          <div className="float-left m-3 absolute">
+            <p className=" text-lg">Text Content</p>
+          </div>
+
+          <div className="img mt-12 flex justify-center items-center">
+            <img
+              className="h-36 hover:scale-125 transition"
+              src="https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg"
+              alt="HTML5"
+            />
+          </div>
+
+          <div className="float-right  m-6">
+            <h1>Css</h1>
           </div>
         </div>
       </div>
