2635f987d8b7160260c3f421378e10fd61078697
Alper Demir <105297896+Hololopoto@users.noreply.github.com>
2024-02-17 19:10:56 +0300

    Update image source in Project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -5,7 +5,10 @@ function Project() {
     <div>
       <div className="flex flex-col text-white">
         <button>Details</button>
-        <img src="src/Images/Netflix-Project.png" alt="Netflix-Clone" />
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/5/54/GitHub_Logo.png"
+          alt="Netflix-Clone"
+        />
       </div>
     </div>
   );
