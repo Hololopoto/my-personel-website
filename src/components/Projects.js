fb8f8ed08b97bc614f0f8c4565959e0bdd7de114
Alper Demir <105297896+Hololopoto@users.noreply.github.com>
2024-02-28 13:58:11 +0300

    Add crossword image and update project details

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,7 +13,7 @@ import pokedex2 from "./Images/PokedexInfo2.png";
 import pokedex3 from "./Images/PokedexInfo3.png";
 import pokedex4 from "./Images/PokedexInfo4.png";
 import pokedex5 from "./Images/PokedexInfo5.png";
-
+import crossword from "./Images/Crossword.png";
 function Projects() {
   return (
     <div>
@@ -70,6 +70,53 @@ function Projects() {
         Title={"Pokedex"}
         Link={"https://hololopoto-pokedex.netlify.app/"}
         Img={pokedex}
+        Details={
+          <div className="bg-slate-300 absolute left-[32rem] max-[1024px]:left-[9rem] max-[768px]:left-[4rem] z-50 max-[768px]:w-[70%] max-[768px]:items-center flex flex-col rounded-lg">
+            <Info
+              InfoText={
+                "PokeApi, Axios, React.js and Tailwind were used in this project."
+              }
+              InfoText2={
+                "My goal in this project is to create a pokemon list with the data in the API."
+              }
+            />
+
+            <Info
+              InfoImg={pokedex1}
+              InfoText={"Extracting data from API and defining it to state"}
+            />
+            <Info
+              InfoImg={pokedex2}
+              InfoText={
+                "A different url comes from the API for each pokemon, and the pokemon details are included in the data in this url."
+              }
+              InfoText2={` I requested this URL in the API again and listed them one after another.Pokemon sometimes appeared in mixed order, I used "sort( )" to fix this.`}
+            />
+            <Info
+              InfoImg={pokedex3}
+              InfoText={
+                "I defined a background color that changes depending on the type of pokemon."
+              }
+            />
+
+            <Info
+              InfoImg={pokedex4}
+              InfoText={
+                "I used the images from a different URL because it looked better."
+              }
+              InfoText2={`While the id in the API was 1,2,3, the names corresponding to this URL were 001,002,003. I used "padStart( )" to fix this situation.`}
+            />
+            <Info
+              InfoImg={pokedex5}
+              InfoText={"I wrote the initials of the names in uppercase"}
+            />
+          </div>
+        }
+      />
+      <Project
+        Title={"Crossword Game"}
+        Link={"https://hololopoto-crossword-game.netlify.app"}
+        Img={crossword}
         Details={
           <div className="bg-slate-300 absolute left-[32rem] max-[1024px]:left-[9rem] max-[768px]:left-[4rem] z-50 max-[768px]:w-[70%] max-[768px]:items-center flex flex-col rounded-lg">
             <Info
