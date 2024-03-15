1d4f634454d655298517a5ab989ce73197820e23
Alper Demir <105297896+Hololopoto@users.noreply.github.com>
2024-03-15 16:31:13 +0300

    Add crossword images and update project descriptions

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -14,6 +14,11 @@ import pokedex3 from "./Images/PokedexInfo3.png";
 import pokedex4 from "./Images/PokedexInfo4.png";
 import pokedex5 from "./Images/PokedexInfo5.png";
 import crossword from "./Images/Crossword.png";
+import crossword1 from "./Images/CrosswordInfo1.png";
+import crossword2 from "./Images/CrosswordInfo2.png";
+import crossword3 from "./Images/CrosswordInfo3.png";
+import crossword4 from "./Images/CrosswordInfo4.png";
+import crossword5 from "./Images/CrosswordInfo5.png";
 function Projects() {
   return (
     <div>
@@ -121,38 +126,36 @@ function Projects() {
           <div className="bg-slate-300 absolute left-[32rem] max-[1024px]:left-[9rem] max-[768px]:left-[4rem] z-50 max-[768px]:w-[70%] max-[768px]:items-center flex flex-col rounded-lg">
             <Info
               InfoText={
-                "PokeApi, Axios, React.js and Tailwind were used in this project."
+                "Axios, React.js and Tailwind were used in this project."
               }
-              InfoText2={"My goal"}
+              InfoText2={
+                "The goal in this project was to create a word game by mixing random letters with the letters containing the answer."
+              }
             />
 
             <Info
-              InfoImg={pokedex1}
-              InfoText={"Extracting data from API and defining it to state"}
-            />
-            <Info
-              InfoImg={pokedex2}
+              InfoImg={crossword1}
               InfoText={
-                "A different url comes from the API for each pokemon, and the pokemon details are included in the data in this url."
+                "Split each letter in the answer to the question created in the Data.js file and shuffle the order by adding 4 random letters to these letters."
               }
-              InfoText2={` I requested this URL in the API again and listed them one after another.Pokemon sometimes appeared in mixed order, I used "sort( )" to fix this.`}
             />
+            <Info InfoImg={crossword2} InfoText={"Shuffle function"} />
             <Info
-              InfoImg={pokedex3}
+              InfoImg={crossword3}
               InfoText={
                 "I defined a background color that changes depending on the type of pokemon."
               }
             />
 
             <Info
-              InfoImg={pokedex4}
+              InfoImg={crossword4}
               InfoText={
                 "I used the images from a different URL because it looked better."
               }
               InfoText2={`While the id in the API was 1,2,3, the names corresponding to this URL were 001,002,003. I used "padStart( )" to fix this situation.`}
             />
             <Info
-              InfoImg={pokedex5}
+              InfoImg={crossword5}
               InfoText={"I wrote the initials of the names in uppercase"}
             />
           </div>
