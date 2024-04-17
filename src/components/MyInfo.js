a301311d5a8882c56a8965290907779006bbc82e
Alper Demir <105297896+Hololopoto@users.noreply.github.com>
2024-04-17 18:48:00 +0300

    Update introductory text in MyInfo.js

--- src/components/MyInfo.js
+++ src/components/MyInfo.js
@@ -3,11 +3,11 @@ import React from "react";
 function MyInfo() {
   return (
     <div>
-      <div className="my-infos text-center text-white bg-neutral-500 m-20 p-20 max-[768px]:p-7 max-[768px]:m-10  rounded-md">
+      <div className="my-infos text-center text-white text-lg bg-neutral-500 m-20 p-20 max-[768px]:p-7 max-[768px]:m-10  rounded-md">
         <p>
           Hello, I am Alper. I work as a frontend developer. My skills are
-          listed below. You can then access the details of my projects. My
-          contact information is located at the top left of the page. Hope to
+          listed below. You can then access the details of my projects. <br />{" "}
+          My contact information is located at the top left of the page. Hope to
           see you again.
         </p>
       </div>
