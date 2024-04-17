d4c8cda99e15076050654239703d9da41ccd3d3b
Alper Demir <105297896+Hololopoto@users.noreply.github.com>
2024-04-17 18:41:32 +0300

    Update MyInfo component in src/components/MyInfo.js

--- src/components/MyInfo.js
+++ src/components/MyInfo.js
@@ -4,14 +4,7 @@ function MyInfo() {
   return (
     <div>
       <div className="my-infos text-white bg-neutral-500 m-20 p-20 max-[768px]:p-7 max-[768px]:m-10  rounded-md">
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem ea
-          quasi, odio minus nesciunt, vero excepturi perspiciatis officiis in ut
-          expedita voluptatem quas, quisquam accusantium deleniti sint
-          voluptates quibusdam praesentium commodi debitis fugiat voluptatum
-          ipsam nostrum. Nemo omnis excepturi cumque repudiandae quisquam sint
-          hic officiis quasi, eius quos quibusdam voluptas!
-        </p>
+        <p></p>
       </div>
     </div>
   );
